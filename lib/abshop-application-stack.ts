96794d40b5e094960fc13517d5c40fa3919f3780
Vincent Lesierse <vincent@lesierse.com>
2021-02-24 16:12:10 +0100

    Fix dependency to gateway

--- lib/abshop-application-stack.ts
+++ lib/abshop-application-stack.ts
@@ -13,40 +13,52 @@ export class ABShopApplicationStack extends cdk.Stack {
     super(scope, id);
 
     const { cluster } = props;
+    // Create Application resources
     const application = new KubernetesManifestFile(this, 'ApplicationManifest', { cluster, manifestFile: '../manifests/application.yaml' });
-    new AppMeshGateway(this, 'Gateway', { cluster, namespace: 'abshop', name: 'abshop-gw' }).node.addDependency(application);
+    // Create the App Mesh Gateway
+    const gateway = new AppMeshGateway(this, 'Gateway', { cluster, namespace: 'abshop', name: 'abshop-gw' });
+    gateway.node.addDependency(application);
+    // Create the redis
     new KubernetesManifestFile(this, 'Redis', { cluster, manifestFile: '../manifests/services/redis.yaml' })
       .node.addDependency(application);
+    // Create the image service
     new ABShopService(this, 'ImageService', {
       cluster,
       imageDirectory: '../src/imageservice',
       manifestFile: '../manifests/services/imageservice.yaml'
     }).node.addDependency(application);
+    // Create the catalog service
     new ABShopService(this, 'CatalogService', {
       cluster,
       imageDirectory: '../src/catalogservice',
       manifestFile: '../manifests/services/catalogservice.yaml'
     }).node.addDependency(application);
+    // Create the cart service
     new ABShopService(this, 'CartService', {
       cluster,
       imageDirectory: '../src/cartservice',
       manifestFile: '../manifests/services/cartservice.yaml',
     }).node.addDependency(application);
+    // Create the order service
     new ABShopService(this, 'OrderService', {
       cluster,
       imageDirectory: '../src/orderservice',
       manifestFile: '../manifests/services/orderservice.yaml',
     }).node.addDependency(application);
+    // Create the recommender service
     new ABShopService(this, 'RecommenderService', {
       cluster,
       imageDirectory: '../src/recommenderservice',
       manifestFile: '../manifests/services/recommenderservice.yaml',
     }).node.addDependency(application);
-    new ABShopService(this, 'FrontendService', {
+    // Create the frontend service
+    const frontend = new ABShopService(this, 'FrontendService', {
       cluster,
       imageDirectory: '../src/frontend',
       manifestFile: '../manifests/services/frontend.yaml',
       manifestContainerName: 'frontend'
-    }).node.addDependency(application);
+    });
+    frontend.node.addDependency(application);
+    frontend.node.addDependency(gateway);
   }
 }
